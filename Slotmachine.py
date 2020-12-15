e49ab31dcf797a605d4eaf5c27cfa397abb06f2c
kaz1m1r <c.haan98@protonmail.com>
2020-12-15 21:59:09 +0100

    Slot machine banner 'slotmachine_banner.txt'

---
+++ Slotmachine.py
@@ -0,0 +1,28 @@
+from Game import Game
+from termcolor import colored
+
+class RowOfSquares:
+    """
+    Colored square class
+    """
+
+    def __init__(self, color_string):
+        self.color = color_string
+
+    def show(self):
+        entire_square = "█"
+        top_square = "▀"
+
+        for _ in range(4):
+            print(colored(10 * entire_square, self.color))
+            print(colored(10 * entire_square, self.color))
+            print(colored(10 * entire_square, self.color))
+            print(colored(10 * entire_square, self.color))
+            print(colored(10 * top_square, self.color))
+
+class Slotmachine(Game):
+
+    def __init__(self, slotmachine_banner):
+        Game.__init__(self, slotmachine_banner)
+        self.welcome_message()
+
