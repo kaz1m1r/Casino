8ef33fa626876827ed6251019ff117a63fb8ba21
kaz1m1r <c.haan98@protonmail.com>
2020-12-16 19:51:37 +0100

    Added functionality of rolling

--- Slotmachine.py
+++ Slotmachine.py
@@ -1,7 +1,9 @@
 from Game import Game
 from termcolor import colored
+import time
 
-class RowOfSquares:
+
+class Square:
     """
     Colored square class
     """
@@ -20,6 +22,7 @@ class RowOfSquares:
             print(colored(10 * entire_square, self.color))
             print(colored(10 * top_square, self.color))
 
+
 class Slotmachine(Game):
 
     def __init__(self, slotmachine_banner):
@@ -30,5 +33,31 @@ class Slotmachine(Game):
 
         self.selected_squares = []
         self.square_colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+        self.roll()
 
+    def roll(self):
+        """
+        Simulating rolling
+        :return: None
+        """
 
+        while True:
+            for color in self.square_colors:
+                self.print_square(color)
+                time.sleep(0.05)
+
+    def print_square(self, color):
+        """
+        Method for printing a square of color 'color'
+        :param color:
+        :return: None
+        """
+
+        entire_square = "█"
+        top_square = "▀"
+
+        print(colored(10 * entire_square, color))
+        print(colored(10 * entire_square, color))
+        print(colored(10 * entire_square, color))
+        print(colored(10 * entire_square, color))
+        print(colored(10 * top_square, color))
